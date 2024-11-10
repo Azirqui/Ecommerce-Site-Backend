aba7db64a0f748142a1730d2b4b6e5583cc534fd
Ch. Noman Ahmad <mr.noman0095@gmail.com>
2024-11-10 17:19:32 +0500

    Commited

--- api/index.js
+++ api/index.js
@@ -1,4 +1,4 @@
-const app = require('../app'); // Adjust the path if necessary
+const app = require('../server'); // Adjust the path if necessary
 const { createServer } = require('@vercel/node');
 
 module.exports = createServer(app);
